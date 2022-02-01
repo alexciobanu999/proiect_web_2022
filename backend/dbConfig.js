b238ebd708775a192a5dc61972f24b1c3a0a0d44
alexciobanu999 <ciobanualexandru17@stud.ase.ro>
2022-02-01 14:25:29 +0200

    "commit"

--- backend/dbConfig.js
+++ backend/dbConfig.js
@@ -4,8 +4,8 @@ import { DB_USERNAME, DB_PASSWORD } from './Const.js'
 const db = new Sequelize({
     dialect: 'mysql',
     database: 'Proiect',
-    username: DB_USERNAME,
-    password: DB_PASSWORD,
+    username: DB_USERNAME, //username: 'root'
+    password: DB_PASSWORD,  //password: 'root'
     logging: false,
     define: {
         timestamps: false,
